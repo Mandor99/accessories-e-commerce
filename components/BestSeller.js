7beef72bbb105701309bc7b261c3a3114458f330
Mandor99 <mahmoudmandor65@gmail.com>
2022-12-16 05:34:53 +0200

    get data from sanity

--- components/BestSeller.js
+++ components/BestSeller.js
@@ -1,28 +1,30 @@
 import Image from 'next/legacy/image'
+import Link from 'next/link'
+import { urlFor } from '../lib/client'
 
 
-function BestSeller() {
+function BestSeller({products}) {
   return (
     <section className='mt-16'>
         <div className="container">
             <div className=''>
                 <h3 className='text-center text-my-gray-200 font-bold text-[2rem] md:text-5xl capitalize mb-3'>best seller products</h3>
-                <p className='text-center text-my-gray capitalize text-sm md:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+                <p className='text-center text-my-gray capitalize text-sm md:text-base'>there are many variation passages</p>
             </div>
             <section className='flex justify-center gap-4 mt-8 flex-col items-center md:items-start md:flex-row flex-wrap'>
                 {
-                    ['speaker1', 'headphone1', 'headphones_a_1', 'earphones_a_1', 'watch_3', 'earphones_b_1', 'headphones_b_1', 'earphones_c_1'].map((product, i) => (
-                       <div key={i}>
+                    products.map(({_id, image, smallDetails, price, slug}, i) => (
+                       <Link href={`/product/${slug.current}`} key={_id}>
                          <div className='relative rounded-md bg-my-light-200 flex items-center justify-center cursor-pointer'>
                             <figure className="relative w-[55vw] shadow-md md:w-[29vw] md:max-w-[35vw] lg:w-[15vw] lg:max-w-[15vw] h-[200px]">
-                                <Image src={`/images/${product}.webp`} className="top-0 left-0 absolute object-contain" alt='product image' layout='fill'/>
+                                <Image src={urlFor(image[0]).url()} className="top-0 left-0 absolute object-contain" alt='product image' layout='fill'/>
                             </figure>
                         </div>
                         <div className='mt-2 text-center md:text-start'>
-                            <p className='text-my-gray-100'>Lorem ipsum dolor.</p>
-                            <h5 className='font-[900]'>$50</h5>
+                            <p className='text-my-gray-100'>{smallDetails}</p>
+                            <h5 className='font-[900]'>{price}</h5>
                         </div>
-                       </div>
+                       </Link>
                     ))
                 }
             </section>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import Banner from '../components/Banner'
 import BestSeller from '../components/BestSeller'
 import SaleBanner from '../components/SaleBanner'
+import { client } from '../lib/client'
 
-export default function Home() {
+export default function Home({products, bannerData}) {
+
   return (
     <>
-      <Banner />
-      <BestSeller />
-      <SaleBanner />
+      <Banner bannerData={bannerData[1]} />
+      <BestSeller products={products} />
+      <SaleBanner  bannerData={bannerData[0]} />
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const bannerData = await client.fetch(`*[_type=="banner"]`)
+  const products = await client.fetch(`*[_type=="product"]`)
+  return {
+    props: {bannerData, products}
+  }
 }
