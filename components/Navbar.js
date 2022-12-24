0222d66c1a83b5694105b85b8f26d8ba89d9213b
Mandor99 <mahmoudmandor65@gmail.com>
2022-12-24 04:03:08 +0200

    edit logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import {AiOutlineShopping} from 'react-icons/ai'
 import Cart from './Cart'
 import {useCart} from '../context/cartContext'
+import Link from 'next/link'
 
 const Navbar = () => {
   const {toggleCart, setToggleCart, stateCart} = useCart()
 
   return (
     <nav className='flex py-4 px-8 justify-between sticky top-0 shadow-sm text-my-gray z-50 bg-white'>
-        <h1 className="uppercase">mobShop</h1>
+        <Link href={'/'} className="uppercase cursor-pointer">mobShop</Link>
         <div className='cursor-pointer relative' onClick={() => setToggleCart(true)}>
             <AiOutlineShopping className='text-2xl'/>
             <span className='absolute text-white bg-my-red rounded-[50%] w-6 h-6 text-center -top-3 left-3 text-xs leading-[initial] flex items-center justify-center'>{stateCart.cart.length}</span>
